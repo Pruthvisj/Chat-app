c072faa0b17cdb729a23b568115c7a2ce62da2fd
Pruthvisj <89569955+Pruthvisj@users.noreply.github.com>
2025-09-30 20:00:11 +0530

    Added Login and Logout function sing jwt
    Implemented user login and logout functionality with JWT-based authentication.
    Handled secure cookie creation on login and proper cookie clearing on logout.

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 //here we define the logics
 import bcrypt from "bcryptjs";
+import createTokenAndSaveCookie from "../jwt/generateToken.js";
 import User from "../models/user.model.js";
 
 export const signUp = async (req, res) => {
@@ -17,13 +18,48 @@ export const signUp = async (req, res) => {
     //Hashing the passowrd
     const hashPassword = await bcrypt.hash(password, 10);
     const newUser = await new User({ name, email, password: hashPassword });
-    await newUser
-      .save()
-      .then(() =>
-        res.status(201).json({ message: "User resgisted successfully" })
-      );
+    await newUser.save();
+    if (newUser) {
+      await createTokenAndSaveCookie(newUser._id, res);
+      res.status(201).json({ message: "User resgisted successfully", newUser });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(404).json({ message: "Invalid User or Passsword" });
+    }
+    createTokenAndSaveCookie(user._id, res);
+    res.status(201).json({
+      message: "User logged in Successfully",
+      user: { 
+        _id: user._id, 
+        name: user.name, 
+        email: user.email 
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+export const logout = async (req, res)=> {
+  //we'll clear the cookie when user click on logout
+  try {
+    res.clearCookie('jwt');
+    // res.clearCookie('token'); // this will also work.
+    res.status(200).json({message:"User logged out Successfully"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message:"server error"});
+  }
+}
--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { signUp } from "../controller/user.controller.js";
+import { login, logout, signUp } from "../controller/user.controller.js";
 const router = express.Router();
 
 router.post("/signup",signUp)
+router.post("/login", login)
+router.post("/logout", logout)
 export default router;
